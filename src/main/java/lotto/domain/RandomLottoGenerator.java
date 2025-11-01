a94e2c083de0eff7e03c5f371484a85e24ba1c87
Junyou Park <pjy008008@gmail.com>
2025-11-02 00:12:28 +0900

    fix(RandomLottoGenerator): ApplicationTest 통과를 위한 정렬 로직 수정
    - 로또 번호 생성 시 Collections.sort 제거
    - sorted()를 사용해 정렬된 리스트 바로 생성

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -2,15 +2,16 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collections;
 import java.util.List;
 
 import static lotto.util.LottoConstants.*;
 
 public class RandomLottoGenerator implements LottoGenerator {
     public Lotto generate() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT);
-        Collections.sort(lotto);
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT)
+                .stream()
+                .sorted()
+                .toList();
         return new Lotto(lotto);
     }
 }
