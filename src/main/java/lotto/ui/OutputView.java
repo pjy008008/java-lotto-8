ca54508be2a179d7f7f868f7cc39cd5869d1b341
Junyou Park <pjy008008@gmail.com>
2025-11-01 22:42:24 +0900

    refactor(OutputView): 하드코딩 문자열 제거 및 상수화

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -9,20 +9,27 @@ import java.util.*;
 import static lotto.domain.LottoRank.*;
 
 public class OutputView {
+    private static final String PURCHASED_LOTTO_MESSAGE = "\n%d개를 구매했습니다.";
+    private static final String RESULT_HEADER = "\n당첨 통계\n---";
+    private static final String RESULT_FORMAT = "%s (%,d원) - %d개";
+    private static final String PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
     public void printPurchasedLotto(PurchasedLotto purchasedLotto) {
         int ticketAmount = purchasedLotto.getTicketAmount();
-        System.out.println("\n" + ticketAmount + "개를 구매했습니다.");
+        String displayFormat = String.format(PURCHASED_LOTTO_MESSAGE,
+                ticketAmount);
+        System.out.println(displayFormat);
         for (Lotto lotto: purchasedLotto.getPurchasedLotto()) {
             System.out.println(lotto);
         }
     }
 
     public void printResult(Map<LottoRank, Integer> result) {
-        System.out.println("\n당첨 통계\n---");
+        System.out.println(RESULT_HEADER);
         List<LottoRank> ranksToDisplay = new ArrayList<>(
                 List.of(FIFTH, FOURTH, THIRD, SECOND, FIRST));
         for (LottoRank rank : ranksToDisplay) {
-            String displayFormat = String.format("%s (%,d원) - %d개",
+            String displayFormat = String.format(RESULT_FORMAT,
                     rank.getDescription(),
                     rank.getPrizeMoney(),
                     result.get(rank)
@@ -36,7 +43,7 @@ public class OutputView {
         for (LottoRank rank : result.keySet()) {
             sum += rank.getPrizeMoney() * result.get(rank);
         }
-        String displayFormat = String.format("총 수익률은 %.1f%%입니다.",
+        String displayFormat = String.format(PROFIT_MESSAGE,
                 ((double) sum / purchaseAmount) * 100);
         System.out.println(displayFormat);
     }
