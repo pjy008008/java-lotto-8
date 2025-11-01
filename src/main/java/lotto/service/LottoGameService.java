e42817e980c90f6606f905d44daa08347494743c
Junyou Park <pjy008008@gmail.com>
2025-11-01 20:01:49 +0900

    feat(LottoGameService): 로또 결과 계산 기능 추가
    - `calculateResult`: 구매한 로또와 당첨 번호, 보너스 번호를 비교하여 LottoRank별 당첨 횟수 집계

---
+++ src/main/java/lotto/service/LottoGameService.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.PurchasedLotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoGameService {
+
+    public Map<LottoRank, Integer> calculateResult(
+            PurchasedLotto purchasedLotto,
+            Lotto winningLotto,
+            int bonusNumber
+    ) {
+        Map<LottoRank, Integer> results = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            results.put(rank, 0);
+        }
+        for (Lotto lotto : purchasedLotto.getPurchasedLotto()) {
+            int matchCount = lotto.calculateMatchCount(winningLotto);
+            boolean bonusMatch = lotto.has(bonusNumber);
+            LottoRank rank = LottoRank.valueOf(matchCount, bonusMatch);
+            results.put(rank, results.get(rank) + 1);
+        }
+        return results;
+    }
+}
