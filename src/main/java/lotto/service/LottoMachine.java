9d3ae75625039eefa8b6179f48de8d4c390c5057
Junyou Park <pjy008008@gmail.com>
2025-11-01 18:04:30 +0900

    refactor(LottoMachine): LottoGenerator 주입 및 로또 발행 로직 스트림으로 간결화

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -1,19 +1,25 @@
 package lotto.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
 import lotto.domain.PurchasedLotto;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoMachine {
+    private final LottoGenerator lottoGenerator;
+
+    public LottoMachine(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
     PurchasedLotto issueTickets(int purchaseAmount) {
         int ticketAmount = purchaseAmount / 1000;
-        List<Lotto> purchaseLotto = new ArrayList<>();
-        for (int i = 0; i < ticketAmount; i++) {
-            purchaseLotto.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-        }
+        List<Lotto> purchaseLotto = IntStream.range(0, ticketAmount)
+                .mapToObj(i -> lottoGenerator.generate())
+                .collect(Collectors.toList());
         return new PurchasedLotto(purchaseLotto);
     }
 }
