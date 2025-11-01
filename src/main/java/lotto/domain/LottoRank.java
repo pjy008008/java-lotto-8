9c0d0ef3d00c306b756b6abcddcb2ff21ce67c0f
Junyou Park <pjy008008@gmail.com>
2025-11-01 20:57:46 +0900

    feat(LottoRank): description 추가 및 getter 메서드 추가
    - `getPrizeMoney`: 각 등수의 상금 조회 기능 추가
    - `getDescription`: 각 등수 설명 조회 기능 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,19 +1,29 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST(6, 2_000_000_000L),
-    SECOND(5, 30_000_000L),
-    THIRD(5, 1_500_000L),
-    FOURTH(4, 50_000L),
-    FIFTH(3, 5_000L),
-    NONE(0, 0L); // 0, 1, 2개 일치 시
+    FIRST(6, 2_000_000_000L,"6개 일치"),
+    SECOND(5, 30_000_000L,"5개 일치, 보너스 볼 일치"),
+    THIRD(5, 1_500_000L,"5개 일치"),
+    FOURTH(4, 50_000L,"4개 일치"),
+    FIFTH(3, 5_000L,"3개 일치"),
+    NONE(0, 0L,"2개 이하 일치");
 
     private final int matchCount;
     private final long prizeMoney;
+    private final String description;
 
-    LottoRank(int matchCount, long prizeMoney) {
+    LottoRank(int matchCount, long prizeMoney, String description) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.description = description;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public static LottoRank valueOf(int matchCount, boolean bonusMatch) {
