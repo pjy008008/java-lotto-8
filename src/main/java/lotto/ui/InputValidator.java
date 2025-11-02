79241456c5bf5db9d295c56b9c90c410c8691f8b
Junyou Park <pjy008008@gmail.com>
2025-11-02 16:24:10 +0900

    refactor(InputValidator): 가독성 향상을 위한 함수 분리

--- src/main/java/lotto/ui/InputValidator.java
+++ src/main/java/lotto/ui/InputValidator.java
@@ -10,21 +10,37 @@ public class InputValidator {
     private static final String ERROR_BONUS_NUMBER_RANGE = "보너스 번호는 " + LOTTO_NUMBER_MIN + "에서 " + LOTTO_NUMBER_MAX + "사이의 숫자여야 합니다.";
     private static final String ERROR_BONUS_NUMBER_DUPLICATE = "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
 
-    private static final int MIN_PURCHASE_AMOUNT = 1;
+    private static final int POSITIVE_THRESHOLD = 1;
 
     public void validatePurchaseAmount(int purchaseAmount) {
-        if (purchaseAmount < MIN_PURCHASE_AMOUNT) {
+        validatePositive(purchaseAmount);
+        validateUnit(purchaseAmount);
+    }
+
+    private static void validatePositive(int purchaseAmount) {
+        if (purchaseAmount < POSITIVE_THRESHOLD) {
             throw new IllegalArgumentException(ERROR_PURCHASE_AMOUNT_POSITIVE);
         }
+    }
+
+    private static void validateUnit(int purchaseAmount) {
         if (purchaseAmount % PURCHASE_UNIT != 0) {
             throw new IllegalArgumentException(ERROR_PURCHASE_AMOUNT_UNIT);
         }
     }
 
     public void validateBonusNumber(int bonusNumber, Lotto winningNumbers) {
+        validateRange(bonusNumber);
+        validateDuplicate(bonusNumber, winningNumbers);
+    }
+
+    private static void validateRange(int bonusNumber) {
         if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
             throw new IllegalArgumentException(ERROR_BONUS_NUMBER_RANGE);
         }
+    }
+
+    private static void validateDuplicate(int bonusNumber, Lotto winningNumbers) {
         if (winningNumbers.has(bonusNumber)) {
             throw new IllegalArgumentException(ERROR_BONUS_NUMBER_DUPLICATE);
         }
