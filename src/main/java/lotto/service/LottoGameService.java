08c9095aff1f094d15a4bdbc0c75afd32fb45a57
Junyou Park <pjy008008@gmail.com>
2025-11-02 15:53:46 +0900

    refactor(LottoGameService): 가독성 향상을 위한 함수 분리

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -14,11 +14,15 @@ public class LottoGameService {
     ) {
         WinningStat winningStat = new WinningStat();
         for (Lotto lotto : purchasedLotto.purchasedLotto()) {
-            int matchCount = lotto.calculateMatchCount(winningLotto);
-            boolean bonusMatch = lotto.has(bonusNumber);
-            LottoRank rank = LottoRank.valueOf(matchCount, bonusMatch);
-            winningStat.increment(rank);
+            updateWinningStat(winningLotto, bonusNumber, lotto, winningStat);
         }
         return winningStat;
+    }
+
+    private static void updateWinningStat(Lotto winningLotto, int bonusNumber, Lotto lotto, WinningStat winningStat) {
+        int matchCount = lotto.calculateMatchCount(winningLotto);
+        boolean bonusMatch = lotto.has(bonusNumber);
+        LottoRank rank = LottoRank.valueOf(matchCount, bonusMatch);
+        winningStat.increment(rank);
     }
 }
